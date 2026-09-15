1cd658188cb9d32f661fecc03be40db334b047bb
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-15 18:04:16 +0100

    Add CameraX video recording dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,5 +18,6 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.4.1")
     implementation("androidx.camera:camera-lifecycle:1.4.1")
     implementation("androidx.camera:camera-view:1.4.1")
+    implementation("androidx.camera:camera-video:1.4.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
